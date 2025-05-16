46c7cbdf63d2b4353e0e875a26ccdd8fd9eaafd5
Igor Churikov <igorchrkv@gmail.com>
2025-05-16 21:48:37 +0300

    init

---
+++ сore/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class DBSettings(BaseSettings):
+    url: str = f"sqlite+aiosqlite:///./db.sqlite3"
+    echo: bool = True
+
+class Settings(BaseSettings):
+    db: DBSettings 
+
+settings = Settings()
